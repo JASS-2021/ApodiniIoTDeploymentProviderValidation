e9a2036da7f802691d9e6c6b200a5c5e3ff03246
Felix Desiderato <felix.desiderato@tum.de>
2021-12-12 10:03:40 +0100

    Compliance update

--- evaluation_processing.py
+++ evaluation_processing.py
@@ -1,3 +1,11 @@
+#
+# This source file is part of the JASS open source project
+#
+# SPDX-FileCopyrightText: 2019-2021 Paul Schmiedmayer and the JASS project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+#
+# SPDX-License-Identifier: MIT
+#
+
 import os
 import sys
 from datetime import datetime
